e67dc1bc3124284e27e080df191a7deb668ed5ec
issa23i <isabelpastorlopezcv@gmail.com>
2023-05-07 18:32:44 +0200

    hotel detalle

--- src/app/pages/hotel/hotel.component.ts
+++ src/app/pages/hotel/hotel.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { Hotel } from 'src/app/interfaces/hotel';
 import { HotelService } from 'src/app/services/hotel.service';
+import { ImagenService } from '../../services/imagen.service';
+import { Imagen } from 'src/app/interfaces/imagen';
 
 @Component({
   selector: 'app-hotel',
@@ -10,7 +12,7 @@ import { HotelService } from 'src/app/services/hotel.service';
 })
 export class HotelComponent  implements OnInit {
   
-  private hotel : Hotel ={
+  private _hotel: Hotel = {
     nombre: '',
     direccion: '',
     ciudad: '',
@@ -20,12 +22,18 @@ export class HotelComponent  implements OnInit {
     imagenes: [],
     puntuacion_resenas: 1,
     estrellas: 1
-  }
+  };
+  private _imagenes: Imagen[] = [];
+  
   private _id: string = '';
-  private _cargado : boolean = false
+  private _cargado: boolean = false;
+ 
   
 
-  constructor(private hotelService: HotelService, private activatedRoute: ActivatedRoute) { }
+  constructor(private hotelService: HotelService 
+    , private activatedRoute: ActivatedRoute
+    , private imagenService: ImagenService
+    ) { }
 
   ngOnInit() {
     
@@ -36,11 +44,37 @@ export class HotelComponent  implements OnInit {
       this.hotelService.obtenerHotel(this._id)
       .subscribe((resp) => {
         this.hotel = resp.data
-        this._cargado = true
         console.log(this.hotel)
+        this.hotel.imagenes.forEach((idImg) => {
+          this.imagenService.obtenerImagen(idImg)
+          .subscribe((resp)=> {
+            console.log(resp.data.url)
+            this.imagenes.push(resp.data.url)
+          })
+        })
+        this._cargado = true
       })
     })
 
   
+  }
+
+  public get hotel(): Hotel {
+    return this._hotel;
+  }
+  public set hotel(value: Hotel) {
+    this._hotel = value;
+  }
+  public get cargado(): boolean {
+    return this._cargado;
+  }
+  public set cargado(value: boolean) {
+    this._cargado = value;
+  }
+  public get imagenes(): Imagen[] {
+    return this._imagenes;
+  }
+  public set imagenes(value: Imagen[]) {
+    this._imagenes = value;
   }
 }
